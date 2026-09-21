936c730ea479105a2ba883f77d0295be846ac3c2
AxelLab <caxel0624@gmail.com>
2026-09-21 16:31:27 -0500

    feat: Estructura inicial del proyecto

---
+++ T1-POO/src/main/java/Ejec_Cafeteria/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejec_Cafeteria;
+
+/**
+ *
+ * @author Ingenieria
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
